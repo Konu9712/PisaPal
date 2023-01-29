c2d7c12ac2186668edf0b7bf79b342d63a460cc5
Sagar Vekariya <sagarvekariya005@gmail.com>
2023-01-29 15:21:07 -0500

    Create Login and Register Page

---
+++ src/components/login.component.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Login</h3>
+
+        <div className="mt-3 mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+       
+      </form>
+    )
+  }
+}
