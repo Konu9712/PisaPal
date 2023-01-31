63c8775bbddeab64f5a5624cfddeb5d03e2d85aa
Konu <51238256+Konu9712@users.noreply.github.com>
2023-01-31 12:56:13 -0500

    handles input in signup form and give appropriate errors

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e, type) => {
+    e.preventDefault();
+    console.log(type);
+    setUser({ ...user, [type]: e.target.value });
+  };
+
+  const onSubmitBtn = (e) => {
+    e.preventDefault();
+    if (user.password !== user.cpassword) {
+      setError("Password and Confirm Password must be same");
+    } else {
+      console.log("Success");
+      setError("");
+    }
+  };
+
   return (
     <div>
       <form>
@@ -8,7 +32,13 @@ export const Signup = () => {
 
         <div className="mt-3 mb-3">
           <label>Name</label>
-          <input type="text" className="form-control" placeholder="Name" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            value={user.name}
+            onChange={(e) => handleSubmit(e, "name")}
+          />
         </div>
 
         <div className="mb-3">
@@ -17,6 +47,8 @@ export const Signup = () => {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            value={user.email}
+            onChange={(e) => handleSubmit(e, "email")}
           />
         </div>
 
@@ -26,6 +58,8 @@ export const Signup = () => {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            value={user.password}
+            onChange={(e) => handleSubmit(e, "password")}
           />
         </div>
 
@@ -35,11 +69,18 @@ export const Signup = () => {
             type="password"
             className="form-control"
             placeholder="Enter Confirm password"
+            value={user.cpassword}
+            onChange={(e) => handleSubmit(e, "cpassword")}
           />
         </div>
+        {error && <div className="alert alert-danger">{error}</div>}
 
         <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => onSubmitBtn(e)}
+          >
             Sign Up
           </button>
         </div>
